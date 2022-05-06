e04ace24227120f30d7eda46281a57ac820aba33
Vronic20 <vronic20@gmail.com>
2022-05-06 17:59:45 -0500

    'update_views'

--- recycleappupc/app/src/main/java/upc/pe/recycleappupc/Information_plastic.kt
+++ recycleappupc/app/src/main/java/upc/pe/recycleappupc/Information_plastic.kt
@@ -10,7 +10,7 @@ class Information_plastic : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information_plastic)
 
-        var btnInfo = this.findViewById<Button>(R.id.btnInfo)
+        var btnInfo = this.findViewById<Button>(R.id.button4)
 
         btnInfo.setOnClickListener{
             val intent = Intent(this@Information_plastic, PlasticActivity::class.java)
---
+++ recycleappupc/app/src/main/java/upc/pe/recycleappupc/collector_points.kt
@@ -0,0 +1,11 @@
+package upc.pe.recycleappupc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class collector_points : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collector_points)
+    }
+}
